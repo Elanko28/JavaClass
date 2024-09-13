1b61e2f5de0b7ad61178875077388345a5d89342
elanko28 <elanko28@gmail.com>
2024-09-13 12:18:48 +0530

    Code added

---
+++ src/com/practices/Sample.java
@@ -0,0 +1,48 @@
+package com.practices;
+
+import java.util.*;
+
+public class Sample {
+
+	// method syntax
+	private void studentId() {
+		System.out.println("hello friends");
+	}
+	
+//	@Override
+	public void employeeName() {
+	System.out.println("employee name is ashok");
+}
+//@Override
+	public void employeeId() {
+//	super.employeeId();
+	System.out.println("employee id is 5678");
+		
+	}
+
+
+	public static void main(String[] args) {
+
+		// objectification syntax
+
+		Sample s = new Sample();
+
+		// calling of method syntax
+
+		s.studentId();
+
+		Scanner s1 = new Scanner(System.in);
+		
+		int id=s1.nextInt();
+		
+		String name=s1.nextLine();
+		
+		System.out.println("my student name is " +name);
+		
+		// next, nextLine need different object
+		
+
+
+
+	}
+}
